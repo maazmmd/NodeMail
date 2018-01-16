e9a997332b98e6835a9d4c7f0e743fb52337fdd2
Mohammed Maaz <maazmmd@gmail.com>
2018-01-16 15:14:14 +0530

    Removing SMTP details and other details

--- nodemail.js
+++ nodemail.js
@@ -3,19 +3,19 @@ const nodemailer = require('nodemailer');
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-    	host: 'noora4.yourpersonalhost.com',
+    	host: 'hostname',
     	port: 465,
         secure: true, // true for 465, false for other ports / false for 567 
         auth: {
-            user: 'info@projectsmiletrust.com', // generated ethereal user
-            pass: 'psinfologin'  // generated ethereal password
+            user: 'username', // generated ethereal user
+            pass: 'password'  // generated ethereal password
         }
     });
 
     // setup email data with unicode symbols
     let mailOptions = {
-        from: '"Project Smile 👻" <info@projectsmiletrust.com>', // sender address
-        to: 'Mohammed Maaz, maazmmd@gmail.com', // list of receivers
+        from: '"From 👻" <from@host.com>', // sender address
+        to: '"To 👻" <to@host.com>', // list of receivers
         subject: 'Hello ✔', // Subject line
         text: 'Hello world?', // plain text body
         html: '<b>Hello world Test Email from Node JS?</b>' // html body
